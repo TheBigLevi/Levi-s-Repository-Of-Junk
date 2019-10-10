148c82c952b858815ff74aade12fc570be9c3fd7
Lachlan Read <lachlan-read@protonmail.com>
2019-10-11 09:32:16 +1030

    Extinction Project
    Addition of the Extinction Project, along with corrosponding scripts

---
+++ Extinction/Assets/Scripts/CurrentEquipment.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CurrentEquipment : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] m_WeaponArray;
+
+    [SerializeField]
+    private GameObject m_AmmoDisplay;
+
+    [SerializeField]
+    private GameObject m_CurrentWeapon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        m_WeaponArray = new GameObject[2];
+
+        int arrayPos = 0;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log(m_WeaponArray[arrayPos]); //Prints current value to console.
+            if (arrayPos >= m_WeaponArray.Length - 1)
+            {
+                arrayPos = 0;
+                m_CurrentWeapon = m_WeaponArray[arrayPos];
+            }
+            else
+            {
+                arrayPos += 1;
+                m_CurrentWeapon = m_WeaponArray[arrayPos];
+            }
+
+            //m_CurrentWeapon
+
+            //InternalBellyRub = BellyRubCount;
+            //BellyRubDisplay.GetComponent<Text>().text = "Belly Rubs: " + InternalBellyRub;
+        }
+    }
+}
+
---
+++ Extinction/Assets/Scripts/WeaponRanagedBehaviour.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponRanagedBehaviour : MonoBehaviour
+{
+    private GameObject m_ThisGun;
+
+    [Header("Weapon Specifics")]
+    [Tooltip("m_Projectile needs to be a child of this object and named 'Projectile'")]
+    [SerializeField]
+    private GameObject m_Projectile;
+
+    [Tooltip("m_FireTransform needs to be a child of this object and named 'FireTransformObject'")]
+    [SerializeField]
+    private GameObject m_FireTransform;
+
+    [SerializeField]
+    //[Tooltip()]
+    private int m_MaxAmmo = 0;
+
+    //[SerializeField]
+    private int m_CurrentAmmo = 0;
+
+    [SerializeField]
+    private int m_ClipSize = 0;
+
+    //[SerializeField]
+    private int m_ClipAmmo = 0;
+
+    [Header("Projectile Properties")]
+    [SerializeField]
+    private float m_Speed = 0f;
+
+    [SerializeField]
+    private float m_Damage;
+
+    [Header("Fire Properties")]
+    [SerializeField]
+    private bool m_CanFire = true;
+
+    [SerializeField]
+    private bool m_IsReloading = false;
+
+    private GameObject m_FiredProjectile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        m_CurrentAmmo = m_MaxAmmo;
+        m_ClipAmmo = m_ClipSize;
+
+        //m_FireTransform = transform.Find("FireTransformObject").gameObject;
+        //m_Projectile = transform.Find("Projectile").gameObject;
+
+        onEquip();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        onFire();
+
+        onReload();
+
+    }
+
+    private void onEquip()
+    {
+        //Set player's current item to this and replace it with current weapon if inventory full
+        //gameObject.GetComponentInParent<CurrentEquipment>(). replace with current hand else fill empty hand
+
+    }
+
+    private void onFire()
+    {
+        if (Input.GetButton("Fire1") && m_CanFire == true && m_ClipAmmo != 0)
+        {
+            m_IsReloading = false;
+            
+
+            m_FiredProjectile = Instantiate(m_Projectile, m_FireTransform.transform.position, m_FireTransform.transform.rotation) as GameObject;
+            Rigidbody m_FiredProjectileRigidbody = m_FiredProjectile.GetComponent<Rigidbody>();
+            m_FiredProjectileRigidbody.AddForce(m_FiredProjectile.transform.forward * m_Speed /* Time.deltaTime*/);
+            
+
+            m_ClipAmmo -= 1;
+        }
+        else if (m_IsReloading == true)
+        {
+            //Stop reload function and add some sort of animation
+        }
+    }
+
+    private void onReload()
+    {
+        if (Input.GetKeyDown(KeyCode.R) && m_CurrentAmmo != 0 && m_ClipAmmo != m_ClipSize)
+        {
+            m_IsReloading = true;
+            int reloadAmount;
+
+            if (m_CanFire == true)
+            {
+                reloadAmount = m_ClipSize - m_ClipAmmo;
+
+                m_CurrentAmmo -= reloadAmount;
+                m_ClipAmmo += reloadAmount;
+
+            }
+
+        }
+    }
+}
